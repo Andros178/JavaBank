7fb42259b9bf5fa736c504857a5d7541e2d2df8c
Andros178 <serdaniel01@gmail.com>
2026-01-29 13:22:09 -0500

    Add: Conexión con base de datos

--- src/main/java/com/example/bank/cuenta/dtos/CuentaCreateDTO.java
+++ src/main/java/com/example/bank/cuenta/dtos/CuentaCreateDTO.java
@@ -1,6 +1,6 @@
 package com.example.bank.cuenta.dtos;
 
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Size;
